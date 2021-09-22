007ae189734b9afcb624f3e3381988d21b0f01a5
RealBauHD <bauhd@gmx.net>
2021-09-22 16:22:31 +0200

    Created config loader

--- system/src/main/java/de/bauhd/system/ServerSystem.java
+++ system/src/main/java/de/bauhd/system/ServerSystem.java
@@ -2,10 +2,17 @@ package de.bauhd.system;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
 public class ServerSystem extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        this.loadConfig();
 
         this.registerCommands();
         this.registerEvents();
@@ -23,6 +30,20 @@ public class ServerSystem extends JavaPlugin {
 
     private void registerEvents() {
 
+    }
+
+    public void loadConfig() {
+        if (!this.getDataFolder().exists()) this.getDataFolder().mkdir();
+
+        final File file = new File(getDataFolder(), "config.yml");
+
+        if (!file.exists()) {
+            try (final InputStream inputStream = this.getResource("config.yml")) {
+                Files.copy(inputStream, file.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
